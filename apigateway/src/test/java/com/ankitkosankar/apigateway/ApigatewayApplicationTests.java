db39fd7437c16b4cb08272277a9d5d496312ccfc
Ankit-Kosankar <53865903+Ankit-Kosankar@users.noreply.github.com>
2025-02-11 01:03:22 +0530

    added a dockerfile in eurekaserver

---
+++ apigateway/src/test/java/com/ankitkosankar/apigateway/ApigatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ankitkosankar.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApigatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
