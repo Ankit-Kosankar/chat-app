ef8c0563d2c683fc86725635c791602fc22c7cc7
Ankit-Kosankar <53865903+Ankit-Kosankar@users.noreply.github.com>
2025-02-20 01:35:55 +0530

    added the api gateway configuration it is working now dont know just refreshed the gradle project and then added the isntance to register with server explicitly rest how is it working its still mystery

--- chatservice/src/main/java/com/ankitkosankar/chatservice/model/Message.java
+++ chatservice/src/main/java/com/ankitkosankar/chatservice/model/Message.java
@@ -2,14 +2,22 @@ package com.ankitkosankar.chatservice.model;
 
 import java.time.LocalDateTime;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Table(name = "message")
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
 public class Message {
 
 	 	@Id
--- chatservice/src/main/java/com/ankitkosankar/chatservice/model/User.java
+++ chatservice/src/main/java/com/ankitkosankar/chatservice/model/User.java
@@ -1,5 +1,19 @@
 package com.ankitkosankar.chatservice.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Table(name="user")
+@Entity
 public class User {
 
+	@Id
+ 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+	
 }
