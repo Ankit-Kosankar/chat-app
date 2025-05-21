8d6d3708a7a0e5adb481bbd863f4d959a742ec7b
Ankit-Kosankar <53865903+Ankit-Kosankar@users.noreply.github.com>
2025-05-21 10:07:45 +0530

    docker compose test

--- userservice/src/main/java/com/ankitkosankar/userservice/controller/Test.java
+++ userservice/src/main/java/com/ankitkosankar/userservice/controller/Test.java
@@ -6,9 +6,11 @@ import java.util.Map;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/users")
 public class Test {
 
 
